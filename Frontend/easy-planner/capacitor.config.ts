4a64398fb34275915ed44e02ba4aa2a00ccf6b4d
Aurélien <aurelienpenotperbet@gmail.com>
2025-05-25 17:17:01 +0200

    feat: init front

---
+++ Frontend/easy-planner/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Easy Planner',
+  webDir: 'www'
+};
+
+export default config;
